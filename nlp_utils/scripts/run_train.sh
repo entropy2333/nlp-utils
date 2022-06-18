7f0389bc30c9888cd3e3f27f3ca6a2c6d107862b
entropy2333 <40735723+entropy2333@users.noreply.github.com>
2022-06-18 23:55:44 +0800

    scripts & parser

---
+++ nlp_utils/scripts/run_train.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -x
+set -e
+if [ ! -n "$1" ]; then
+	echo "Please input tag."
+	exit 0
+fi
+TAG=$1
+DATE=`date +%Y%m%d_%H%M%S`
+nohup python -u \
+    main.py \
+    --bert_dir hfl/chinese-roberta-wwm-ext \
+    --savedmodel_path save/${DATE}_${TAG} \
+    --max_epochs 5 \
+    --batch_size 32 \
+    --model_type bert \
+    --learning_rate 0.00005 \
+    --bert_learning_rate 0.00002 \
+    --warmup_ratio 0.1 \
+    --val_ratio 0.1 \
+    --bert_seq_length 256 \
+    --fc_size 768 \
+    --dropout 0.1 \
+    --grad_clip 1.0 \
+    > logs/train_${DATE}_${TAG}.log 2>&1 &
