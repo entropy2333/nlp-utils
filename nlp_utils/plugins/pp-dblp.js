5f7ce63dac29e905ab36db4ff7d42a9da29a6424
entropy2333 <40735723+entropy2333@users.noreply.github.com>
2022-08-27 22:55:24 +0800

    update dblp

---
+++ nlp_utils/plugins/pp-dblp.js
@@ -0,0 +1,51 @@
+// ==UserScript==
+// @name         dblp-bibtex
+// @namespace    http://tampermonkey.net/
+// @version      0.2
+// @description  add bibtex info on PapersWithCode
+// @author       You
+// @match        https://paperswithcode.com/paper/*
+// @icon         https://paperswithcode.com/favicon.ico
+// @grant        GM_xmlhttpRequest
+// @grant        GM_openInTab
+// @require      https://apps.bdimg.com/libs/jquery/2.1.4/jquery.min.js
+// @run-at       document-end
+// @connect      dblp.uni-trier.de
+// @license      GPL-3.0 License
+// ==/UserScript==
+
+(
+    function () {
+        'use strict';
+        $(document).ready(function () {
+            let getBibtexByTitle = function (query, format = 0) {
+                var request_url = `https://dblp.uni-trier.de/search/publ/bibtex${format}?q=${query}`;
+                console.log(request_url);
+                var bibitem = $('a.badge.badge-light:last').clone();
+                bibitem.attr("href", request_url);
+                bibitem.attr("target", "_blank");
+                bibitem.attr("onclick", "");
+                bibitem.children("span:last")[0].innerHTML = "BibTeX";
+                $('a.badge.badge-light:last').after(bibitem);
+            };
+            let getGithubUrl = function (query, format = 0) {
+                var implitem = $('.paper-impl-cell:first').clone();
+                // var codeitem = $('a.code-table-link:first').clone();
+                // var staritem = $('span[data-name=star]:first').clone();
+                var codeitem = implitem.children()[0];
+                console.log(codeitem);
+                var request_url = codeitem.href.replace("github.com", "hub.fastgit.xyz");
+                console.log(request_url);
+                implitem.children()[0].href = request_url;
+                // codeitem.attr("href", request_url);
+                // $('a.code-table-link:first').after(codeitem);
+                // $('span[data-name=star]:first').after(staritem);
+                $('.paper-impl-cell:first').after(implitem);
+            };
+            var title = $("h1").text().trim();
+            console.log(title);
+            title = encodeURIComponent(title);
+            getBibtexByTitle(title);
+            getGithubUrl(title);
+        })
+    })();
